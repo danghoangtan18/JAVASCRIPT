f942087c36a0f307276ab8176fc3c75e480283ae
danghoangtan18 <huckyleap@gmail.com>
2026-08-18 17:16:55 +0700

    update script.js

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,8 +1,13 @@
 'use strict';
 
 // selecting elements
-const score0El = document.querySelectorById('score--0');
-const score1El = document.querySelectorById('score--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,11 +18,43 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden'); 
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0; 
+
 // Rolling dice funtionality
 btnRoll.addEventListener('click', function(){
+
   // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
 
   // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1:if true, switch to next player
-})
+  // 3. Check for rolled 1
+  if(dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+
+});
+
+btnHold.addEventListener('click', function(){
+  // 1. add curent score to active player's score
+  scores[activePlayer] += currentScore;  
+  document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+  // 2.Check if player's score >= 100
+
+  // 3. switch to the next the player 
+});
